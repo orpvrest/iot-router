2ec6dcf2821e739c6d125a77c76d2d1b4795aed5
Sergey Popov <popovss@mai.ru>
2025-11-12 11:44:31 +0300

    fix: Удалить устаревшие конфигурации обновления и очистить файлы сертификатов в скрипте run.sh

--- config/certbot/run.sh
+++ config/certbot/run.sh
@@ -10,16 +10,10 @@ else
   STAGING_FLAG=""
 fi
 
-# Remove stale renewal configs if we are issuing a new lineage
-rm -f "$LE_DIR/renewal/${CERT_NAME}.conf"
-
 certbot certonly --non-interactive --agree-tos \
   --webroot -w /var/www/certbot \
   --cert-name "${CERT_NAME}" --expand \
   -d "${VPN_SNI_DOMAIN}" -d "${SITE_SNI_DOMAIN}" \
-  --email "${LE_EMAIL}" ${STAGING_FLAG}
-
-# Clean up any "-0001" style renewal files and ensure only our main cert remains
-find "$LE_DIR/renewal" -maxdepth 1 -name "${CERT_NAME}-*.conf" -delete
+  --email "${LE_EMAIL}" ${STAGING_FLAG} || true
 
 certbot renew --dry-run ${STAGING_FLAG}
