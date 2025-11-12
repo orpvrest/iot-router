c71fd59e1ca728866a695045eb1bffd95f4ab776
Sergey Popov <popovss@mai.ru>
2025-11-12 11:57:21 +0300

    fix: Удалить устаревшие сертификаты перед выполнением команды certbot

--- config/certbot/run.sh
+++ config/certbot/run.sh
@@ -10,10 +10,13 @@ else
   STAGING_FLAG=""
 fi
 
+# Remove stale lineages with the same cert name
+rm -rf "$LE_DIR/live/${CERT_NAME}" "$LE_DIR/archive/${CERT_NAME}" "$LE_DIR/renewal/${CERT_NAME}.conf"
+
 certbot certonly --non-interactive --agree-tos \
   --webroot -w /var/www/certbot \
-  --cert-name "${CERT_NAME}" --expand \
+  --cert-name "${CERT_NAME}" \
   -d "${VPN_SNI_DOMAIN}" -d "${SITE_SNI_DOMAIN}" \
-  --email "${LE_EMAIL}" ${STAGING_FLAG} || true
+  --email "${LE_EMAIL}" ${STAGING_FLAG}
 
 certbot renew --dry-run ${STAGING_FLAG}
